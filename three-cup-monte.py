d2e0bee0aeb0cd1240bfa1918777cb8569e85e86
Clifford Frempong <cliffordfrimpong69@gmail.com>
2023-01-23 20:42:07 +0000

    made changes to script

--- three-cup-monte.py
+++ three-cup-monte.py
@@ -1,7 +1,5 @@
 from random import shuffle
 
-cup_ball_list = [' ', 'O', ' ']
-
 
 def shuffle_cup_ball_list(cup_ball_list):
     shuffle(cup_ball_list)
@@ -9,19 +7,25 @@ def shuffle_cup_ball_list(cup_ball_list):
 
 
 def user_guess():
-    guess = ''
-    while guess not in ['0', '1', '2']:
-        guess = input("Please enter which index you think the ball is in")
-    return int(guess)
+    player_name = input("Hello Player! What is your name")
+    player_guess = ''
+    while player_guess not in ['0', '1', '2']:
+        player_guess = input("Please enter which index you think the ball is in")
+    return player_name, int(player_guess)
 
 
-def game(shuffle_cup_ball_list, user_guess):
+def game_check(shuffle_cup_ball_list, user_guess, player_name):
     if shuffle_cup_ball_list[user_guess] == 0:
-        print("you win")
+        print(f"You guessed right, {player_name}!")
     else:
-        print("Sorry")
+        print(f"Wrong choice, {player_name}! Try again")
+        print("Take a look at where the ball is. Better luck next time!")
+        print(shuffle_cup_ball_list)
 
-h = shuffle_cup_ball_list(cup_ball_list)
-v = user_guess()
 
-game(h,v)
+cup_ball_list = [' ', 'O', ' ']
+
+shuffled_list = shuffle_cup_ball_list(cup_ball_list)
+player_name, player_guess = user_guess()
+
+game_check(shuffled_list, player_guess, player_name)
